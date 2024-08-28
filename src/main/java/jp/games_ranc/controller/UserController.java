540e9c8b0bc3dcd000699bc11497cd40b2b572c4
ff5026@mym-b.com <ff5026@mym-b.com>
2024-08-28 11:39:08 +0900

    [UPDATE] project ver.1

--- src/main/java/jp/games_ranc/controller/UserController.java
+++ src/main/java/jp/games_ranc/controller/UserController.java
@@ -11,45 +11,49 @@ import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/user")
+@RequestMapping
 public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/find/all")
+    @GetMapping("/user/all")
     public String getAllUsers(Model model) {
         List<User> users = userService.getAllUsers();
         model.addAttribute("users", users);
         return "user";  // 사용자 리스트를 보여주는 뷰 (예: user-list.html)
     }
 
-    @GetMapping("/find/{id}")
+    @GetMapping("/user/{id}")
     public String getUserById(@PathVariable("id") Long id, Model model) {
         User user = userService.findUserById(id);
         model.addAttribute("user", user);
         return "userDetail";  // 사용자 세부 정보를 보여주는 뷰 (예: user-detail.html)
     }
 
-    @GetMapping("/add")
+    // 회원가입할 때 요구사항
+    // 1. 비밀번호 2차 인증(첫번 째 비밀번호와 두번 째 비밀번호가 같을 시 true)
+    // 2. 각 항목마다 예시를 보여줌.
+
+    @GetMapping("/user/add")
     public String addUserForm(Model model) {
         model.addAttribute("user", new User());
         return "signup";  // 회원가입 폼을 보여주는 뷰 (예: signup.html)
     }
 
-    @PostMapping("/add")
+    @PostMapping("/user/add")
     public String addUser(@ModelAttribute("user") User user, Model model) {
         userService.addUser(user);
-        return "redirect:/find/all";  // 사용자 리스트 페이지로 리다이렉트
+        return "redirect:/user/all";  // 사용자 리스트 페이지로 리다이렉트
     }
 
-    @GetMapping("/update/{id}")
+    @GetMapping("/user/update/{id}")
     public String updateUserForm(@PathVariable("id") Long id, Model model) {
         User user = userService.findUserById(id);
         model.addAttribute("user", user);
         return "userEdit";  // 사용자 정보를 수정하는 폼 뷰 (예: user-edit.html)
     }
 
-    @PostMapping("/update/{id}")
+    @PostMapping("/user/update/{id}")
     public String updateUser(@PathVariable("id") Long id, @ModelAttribute("user") User user) {
         User updateUser = userService.findUserById(id);
         updateUser.setEmail(user.getEmail());
@@ -66,7 +70,7 @@ public class UserController {
         return "redirect:/find/" + id;
     }
 
-    @PostMapping("/delete/{id}")
+    @PostMapping("/user/delete/{id}")
     public String deleteUser(@PathVariable("id") Long id, Model model) {
         userService.delete(id);
         return "redirect:/find/all";  // 삭제 후 사용자 리스트 페이지로 리다이렉트
